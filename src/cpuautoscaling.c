5a20adf0b3408ef74fc167c999bffc5ae0a6ab37
pappacena <pappacena@gmail.com>
2020-05-17 20:35:18 -0300

    Calculating idle percent

--- src/cpuautoscaling.c
+++ src/cpuautoscaling.c
@@ -17,6 +17,7 @@
 #include <linux/irqnr.h>
 #include <linux/sched/cputime.h>
 #include <linux/tick.h>
+#include <linux/delay.h>  // msleep
 
 
 MODULE_LICENSE("GPL");
@@ -39,26 +40,83 @@ static u64 get_idle_time(struct kernel_cpustat *kcs, int cpu) {
 	return idle;
 }
 
+static u64 get_iowait_time(struct kernel_cpustat *kcs, int cpu) {
+	u64 iowait, iowait_usecs = -1ULL;
+
+	if (cpu_online(cpu))
+		iowait_usecs = get_cpu_iowait_time_us(cpu, NULL);
+
+	if (iowait_usecs == -1ULL)
+		/* !NO_HZ or cpu offline so we can rely on cpustat.iowait */
+		iowait = kcs->cpustat[CPUTIME_IOWAIT];
+	else
+		iowait = iowait_usecs * NSEC_PER_USEC;
+
+	return iowait;
+}
+
 
 int get_cpu_usage(void) {
     // See fs/proc/stat.c for more details.
-    int i, user = 0, nice = 0, system = 0, idle = 0;
+    int i, k, idle_percent, idle_percent_sum = 0, last_idle_percent, total_cores = 0;
+    u64 total = 0, user = 0, nice = 0, system = 0, 
+        idle = 0, iowait = 0, irq = 0, softirq = 0;
+    u64 last_user = 0, last_nice = 0, last_system = 0, 
+        last_idle = 0, last_iowait = 0, last_irq = 0, last_softirq = 0; 
 
-    for_each_possible_cpu(i) {
-        struct kernel_cpustat kcpustat;
-        u64 *cpustat = kcpustat.cpustat;
+    for(k = 0; k < 2 ; k++) {
+        for_each_online_cpu(i) {
+            total_cores++;
+            struct kernel_cpustat kcpustat;
+            u64 *cpustat = kcpustat.cpustat;
 
-        kcpustat = kcpustat_cpu(i);
+            kcpustat = kcpustat_cpu(i);
 
-        user		+= cpustat[CPUTIME_USER];
-		nice		+= cpustat[CPUTIME_NICE];
-		system		+= cpustat[CPUTIME_SYSTEM];
-        idle += get_idle_time(&kcpustat, i);
-        
-        printk(KERN_INFO "Processor %d -> %d / %d / %d / %d", 
-                i, user, nice, system, idle);
+            user += cpustat[CPUTIME_USER];
+            nice += cpustat[CPUTIME_NICE];
+            system += cpustat[CPUTIME_SYSTEM];
+            idle += get_idle_time(&kcpustat, i);
+            iowait += get_iowait_time(&kcpustat, i);
+            irq	+= cpustat[CPUTIME_IRQ];
+            softirq	+= cpustat[CPUTIME_SOFTIRQ];
+        }
+
+        if (k == 0) {
+            last_user = user;
+            last_nice = nice;
+            last_system = system;
+            last_idle = idle;
+            last_iowait = iowait;
+            last_irq = irq;
+            last_softirq = softirq;
+
+            user = 0;
+            nice = 0;
+            system = 0;
+            idle = 0;
+            iowait = 0;
+            irq = 0;
+            softirq = 0;
+
+            msleep(1000);
+        }
+        else {
+            user = user - last_user;
+            nice = nice - last_nice;
+            system = system - last_system;
+            idle = idle - last_idle;
+            iowait = iowait - last_iowait;
+            irq = irq - last_irq;
+            softirq = softirq - last_softirq;
+        }
     }
-    return idle;
+
+    total = user + nice + system + idle + iowait + irq + softirq;
+    idle_percent = 100 * idle / total;
+    printk(KERN_INFO "idle = %llu / total = %llu", idle, total);
+    printk(KERN_INFO "Processors idle percent average = %d", idle_percent);
+
+    return idle_percent;
 }
 
 static int __init cpuautoscaling_init(void) {
